644b0e1d59c77dc87640c8050e9b27eea04917c1
rodrigovm10 <rv0611200369@gmail.com>
2024-10-08 12:31:32 -0600

    Manejador de errores implementado

--- src/infrastructure/mappers/user.mapper.ts
+++ src/infrastructure/mappers/user.mapper.ts
@@ -1,14 +1,14 @@
-import { CustomError } from '@domain/errors'
+import { BadRequestException, CustomError, ErrorCode } from '@domain/errors'
 import { User } from '@domain/entities'
 
 export class UserMapper {
   static userEntityFromObject(object: { [key: string]: any }) {
     const { id, name, email, password, role } = object
 
-    if (!id) throw CustomError.badRequest('Missing id')
-    if (!name) throw CustomError.badRequest('Missing name')
-    if (!password) throw CustomError.badRequest('Missing password')
-    if (!role) throw CustomError.badRequest('Missing role')
+    if (!id) throw new BadRequestException('Missing id', ErrorCode.MISSING_FIELD)
+    if (!name) throw new BadRequestException('Missing name', ErrorCode.MISSING_FIELD)
+    if (!password) throw new BadRequestException('Missing password', ErrorCode.MISSING_FIELD)
+    if (!role) throw new BadRequestException('Missing role', ErrorCode.MISSING_FIELD)
 
     return new User(id, name, email, password, role)
   }
